b2a7365a82810175fcd2c50b0fb17721c84eba9d
AsundkarPoornima <poornimaasundkar@gmail.com>
2023-08-08 06:55:37 +0000

    Added all files

---
+++ mycode.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+# Sample code using numpy and pandas
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 22]}
+
+df = pd.DataFrame(data)
+print(df)
+
+print("Hello, Poonu!")
